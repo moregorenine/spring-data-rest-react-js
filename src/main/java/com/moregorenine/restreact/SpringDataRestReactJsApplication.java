09865faed9b4cf3ee3833c1d360f94e05d0484e9
moregorenine <moregorenine@gmail.com>
2022-01-14 00:47:08 +0900

    init project

---
+++ src/main/java/com/moregorenine/restreact/SpringDataRestReactJsApplication.java
@@ -0,0 +1,13 @@
+package com.moregorenine.restreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestReactJsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRestReactJsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moregorenine/restreact/SpringDataRestReactJsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moregorenine.restreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRestReactJsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
